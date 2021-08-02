1e58a43900ac752b30770321e37d5d73c50c2610
Gaurav <gvnsaxena@gmail.com>
2021-08-02 11:08:16 +0100

    push webrtc

--- capture.js
+++ capture.js
@@ -1,25 +1,11 @@
 (function() {
-    // The width and height of the captured photo. We will set the
-    // width to the value defined here, but the height will be
-    // calculated based on the aspect ratio of the input stream.
+    let streaming = false;
+    let video = null;
+    let canvas = null;
+    let photo = null;
+    let startbutton = null;
   
-    var width = 320;    // We will scale the photo width to this
-    var height = 0;     // This will be computed based on the input stream
-  
-    // |streaming| indicates whether or not we're currently streaming
-    // video from the camera. Obviously, we start at false.
-  
-    var streaming = false;
-  
-    // The various HTML elements we need to configure or control. These
-    // will be set by the startup() function.
-  
-    var video = null;
-    var canvas = null;
-    var photo = null;
-    var startbutton = null;
-  
-    function startup() {
+    const startup = () => {
       video = document.getElementById('video');
       canvas = document.getElementById('canvas');
       photo = document.getElementById('photo');
@@ -28,7 +14,6 @@
       navigator.mediaDevices.getUserMedia({video:{ facingMode: "environment"}})
       .then(function(stream) {
         video.srcObject = stream;
-        //video.play();
       })
       .catch(function(err) {
         console.log("An error occurred: " + err);
@@ -36,19 +21,10 @@
   
       video.addEventListener('canplay', function(ev){
         if (!streaming) {
-          height = video.videoHeight / (video.videoWidth/width);
-        
-          // Firefox currently has a bug where the height can't be read from
-          // the video, so we will make assumptions if this happens.
-        
-          if (isNaN(height)) {
-            height = width / (4/3);
-          }
-        
-          video.setAttribute('width', width);
-          video.setAttribute('height', height);
-          canvas.setAttribute('width', width);
-          canvas.setAttribute('height', height);
+          video.setAttribute('width', video.videoWidth);
+          video.setAttribute('height', video.videoHeight);
+          canvas.setAttribute('width', video.videoWidth);
+          canvas.setAttribute('height', video.videoHeight);
           streaming = true;
         }
       }, false);
@@ -61,39 +37,28 @@
       clearphoto();
     }
   
-    // Fill the photo with an indication that none has been
-    // captured.
-  
-    function clearphoto() {
-      var context = canvas.getContext('2d');
+    const clearphoto = () => {
+      let context = canvas.getContext('2d');
       context.fillStyle = "#AAA";
       context.fillRect(0, 0, canvas.width, canvas.height);
   
-      var data = canvas.toDataURL('image/png');
+      let data = canvas.toDataURL('image/jpeg');
       photo.setAttribute('src', data);
     }
     
-    // Capture a photo by fetching the current contents of the video
-    // and drawing it into a canvas, then converting that to a PNG
-    // format data URL. By drawing it on an offscreen canvas and then
-    // drawing that to the screen, we can change its size and/or apply
-    // other changes before drawing it.
-  
-    function takepicture() {
-      var context = canvas.getContext('2d');
-      if (width && height) {
-        canvas.width = width;
-        canvas.height = height;
-        context.drawImage(video, 0, 0, width, height);
+    const takepicture = () => {
+      let context = canvas.getContext('2d');
+      if (video.videoWidth && video.videoHeight) {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
       
-        var data = canvas.toDataURL('image/png');
+        let data = canvas.toDataURL('image/jpeg');
         photo.setAttribute('src', data);
       } else {
         clearphoto();
       }
     }
   
-    // Set up our event listener to run the startup process
-    // once loading is complete.
     window.addEventListener('load', startup, false);
   })();
